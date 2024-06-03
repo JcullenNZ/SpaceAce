214b57c7589a5da120a3c734204df8c556f018e3
James Cullen <94792906+JcullenNZ@users.noreply.github.com>
2024-06-03 15:37:25 +0200

    Creating Manager classes
    Creating Manager classes for:
    UI
    -EventSyst
    Audio
    -MusicObj
    -SFXObj
    Game
    HighScore
    -HighScore object
    
    Adding InputSystem

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,101 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
+using System.IO;
+using UnityEditor;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance;
+    private Scene currentScene;
+    private bool IsPaused = false;
+    private string highScoresFP;
+
+    public enum GameState { MainMenu, InGame, Paused, GameOver };
+    public GameState currentState;
+
+    private void Awake()
     {
-        
+        //Ensure singleton
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        currentState = GameState.MainMenu;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Start()
     {
-        
+        Debug.Log("GameManager start");
+        LoadMainMenu();
     }
+
+
+    public void LoadMainMenu()
+    {
+
+        SceneManager.LoadScene("MainMenu");
+        ChangeState(GameState.MainMenu);
+        Debug.Log("Main Menu Loaded");
+    }
+    
+    public void LoadFirstLevel()
+    {
+        try{
+        SceneManager.LoadScene("Level");
+        ChangeState(GameState.InGame);
+        Debug.Log("Level Loaded");
+        }
+        catch(System.Exception e)
+        {
+            Debug.Log("Error: " + e.Message);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+    public void EndGame(int score, string playerName)
+    {
+        if (HighScoreManager.Instance.IsHighScore(score))
+        {
+            HighScoreManager.Instance.AddHighScore(playerName, score);
+        }
+        ChangeState(GameState.GameOver);
+        SceneManager.LoadScene("GameOver");
+    }
+
+    public void PauseGame()
+    {
+        if (currentState == GameState.InGame)
+        {
+            if (IsPaused == false)
+            {
+                IsPaused = true;
+                ChangeState(GameState.Paused);
+            }
+            else
+                IsPaused = false;
+            ChangeState(GameState.InGame);
+        }
+    }
+
+
+    public void ChangeState(GameState newState)
+    {
+        currentState = newState;
+    }
+
 }
+
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,18 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public EventSystem eventSystem;
+    public static UIManager Instance;
+
+    [Header ("---------Main Menu---------")]
+    public GameObject newGameButton;
+    public GameObject settingsButton;
+    public GameObject quitButton;
+
+    [Header ("---------Settings---------")]
+        public GameObject volumeSlider;
+        public GameObject backButton;
+
+    [Header ("------------Views------------")]
+    public GameObject settingsView;
+    public GameObject mainMenuView;
+
+
+    public GameObject highScoreView;
+    public GameObject highScore;
+
+
+    void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        eventSystem = EventSystem.current;
+    }
+    public void StartGame()
+    {
+        GameManager.Instance.LoadFirstLevel();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowSettings()
     {
-        
+        bool isActive = settingsView.activeSelf;
+        //highScoreView.SetActive(isActive);
+        if(!isActive)
+        {
+            eventSystem.SetSelectedGameObject(volumeSlider);
+        }
+        else
+        {
+            eventSystem.SetSelectedGameObject(newGameButton);
+        }
+        mainMenuView.SetActive(isActive);
+        settingsView.SetActive(!isActive);
+
     }
+
+    public void QuitGame()
+    {
+        Debug.Log("Thanks for playing");
+        GameManager.Instance.QuitGame();
+    }
+
+
 }
